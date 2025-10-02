b486a728bd07f0a121f690c9ee8572b99c0dd4f2
Evan Hennessy <git@hennessyevan.com>
2025-10-01 22:50:28 -0400

    style: format code for consistency and readability in ruleToHTML function

--- src/lib/ruleToHTML.ts
+++ src/lib/ruleToHTML.ts
@@ -1,28 +1,25 @@
-import { Temporal } from '@js-temporal/polyfill'
-import { html, type TemplateResult } from 'lit'
-import type { RRuleTemporal } from 'rrule-temporal'
+import { Temporal } from "@js-temporal/polyfill"
+import { html, type TemplateResult } from "lit"
+import type { RRuleTemporal } from "rrule-temporal"
 
-export function ruleToHTML(rule: RRuleTemporal, opts: {} = {}): TemplateResult {
-  const today = Temporal.Now.zonedDateTimeISO().startOfDay()
-  const nextWeek = today
-    .add({ weeks: 1 })
-    .startOfDay()
-    .subtract({ milliseconds: 1 })
+export function ruleToHTML(rule: RRuleTemporal): TemplateResult {
+	const today = Temporal.Now.zonedDateTimeISO().startOfDay()
+	const nextWeek = today.add({ weeks: 1 }).startOfDay().subtract({ milliseconds: 1 })
 
-  const days = rule.between(today, nextWeek, true)
+	const days = rule.between(today, nextWeek, true)
 
-  return html`<div>
-    <h2>Upcoming Events</h2>
-    <ul>
-      ${days.length === 0 ? '<li>No events in the next week</li>' : ''}
-      ${days.map((day) => {
-        return html/*html*/ `<li>
-          ${day.toLocaleString(undefined, {
-            dateStyle: 'long',
-            timeStyle: 'medium',
-          })}
-        </li>`
-      })}
-    </ul>
-  </div>`
+	return html`<div>
+		<h2>Upcoming Events</h2>
+		<ul>
+			${days.length === 0 ? "<li>No events in the next week</li>" : ""}
+			${days.map((day) => {
+				return html/*html*/ `<li>
+					${day.toLocaleString(undefined, {
+						dateStyle: "long",
+						timeStyle: "medium",
+					})}
+				</li>`
+			})}
+		</ul>
+	</div>`
 }
